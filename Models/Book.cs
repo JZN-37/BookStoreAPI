3b8c8e34fea3a99d72a7ba890452efef62bb5450
Saji Shone <shonesaji11@gmail.com>
2021-11-01 21:08:47 +0300

    Added Book.cs to Model folder

---
+++ Models/Book.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStoreAPI.Models
+{
+  public class Book
+  {
+    public int BId { get; set; }
+    public int BCatId { get; set; }
+    public string BTitle { get; set; }
+    public string BISBN { get; set; }
+    public DateTime BYear { get; set; }
+    public double BPrice { get; set; }
+    public string BDesc { get; set; }
+    public double BPosition { get; set; }
+    public int BCount { get; set; }
+    public bool BStatus { get; set; }
+    public string BImgPath { get; set; }
+    public int Norders { get; set; }
+  }
+}
