7961dd9337269f8813d97d6ab9b3e85a0ab616a3
Josen Jose <josenjose37@gmail.com>
2021-11-02 14:41:27 +0530

    Cart

---
+++ Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStoreAPI.Models
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public int UserId { get; set; }
+        public int BId { get; set; }
+        public int BQty { get; set; }
+    }
+}
